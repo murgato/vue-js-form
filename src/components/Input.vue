9e04bbb3eeb9ab312120f9955035262d86407984
Muilo Couto <77131209+murgato@users.noreply.github.com>
2021-11-17 21:02:38 -0300

    init

---
+++ src/components/Input.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Container-Input">
+    <Label class="Label">Nome</Label>
+    <Input class="Input" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+};
+</script>
+
+<style scoped src="./styles.css" />
---
+++ src/views/Resume.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Container">
+    <img
+      class="Banner"
+      src="https://cdn.pixabay.com/index/2021/11/16/06-30-56-841_1440x550.jpg"
+    />
+    <div class="Container-form">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Resume",
+};
+</script>
+
+<style scoped src="./styles.css" />
